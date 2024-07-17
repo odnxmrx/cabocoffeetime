596a3867f5a17cd4e4a879571688dd23124fe721
odnxmrx <armandopinedag@outlook.com>
2024-07-17 10:48:19 -0700

    add header component

--- src/modules/components/Header/Header.jsx
+++ src/modules/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 // import React from "react";
 import styles from "./Header.module.css";
+import cctLogo from '../../../assets/cabo-coffee-time-logo-full-no-bg.png'
 
 function Header() {
   return (<>
     <section className={styles.heroCover}>
+      <div className={styles.heroLeftSpace}>
+        <img src={cctLogo} alt="cabo coffee time logo"/>
+      </div>
       <div className={styles.heroContent}>
-        <h1>its coffee olock</h1>
+        <h1>CABO COFFEE TIME</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         <button>Learn More</button>
       </div>
